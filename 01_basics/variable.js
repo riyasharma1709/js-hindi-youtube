550b0c282ca32612962f32d89184647bb10c3e70
Riya sharma <121916673+riyasharma1709@users.noreply.github.com>
2025-04-18 12:04:52 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345 
+let accountEmail ="abc@gmail.com"
+var accountPassword = "abc12345"
+State = ""
+
+accountEmail = "riya@gmail.com"
+
+console.log(accountId)
+console.table([accountPassword,accountEmail,State])
+/*
+don't use var use only let because of scoupe andfunction
+In const we can't chage value
+*/

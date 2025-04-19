cc3e2bff1ad88c8c7a822ff8d922bfef7f726fe9
Riya sharma <121916673+riyasharma1709@users.noreply.github.com>
2025-04-19 03:49:55 +0000

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as new versions
+let name ="riya"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 to power 53
+//BigInt
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined => 
+//symbol => unique
+//object
+
+console.log(typeof age)
+console.log(typeof null)//opt:object
+console.log(typeof undefined)//opt:undefined
---
+++ 01_basics/03_conversationoperations.js
@@ -0,0 +1,21 @@
+let score = 33 
+console.log(typeof score)
+let sco = "33"
+console.log(typeof sco)
+
+let valueInNumber = Number(sco) //convert string value in number
+console.log(typeof valueInNumber)
+
+//"33" => 33 in number
+//"33abc" => NaN not a number
+//true => 1 false => 0
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//use empty string in boolean gives false
+
+let someNumber = 25
+let strNub = String(someNumber)
+console.log(strNub)
+console.log(typeof strNub)

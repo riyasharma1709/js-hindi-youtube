54e10ba753657331117ca8828c13ce2b1efcdd4d
Riya sharma <121916673+riyasharma1709@users.noreply.github.com>
2025-04-20 04:29:12 +0000

    datatypes

---
+++ 01_basics/datatypessummary.js
@@ -0,0 +1,38 @@
+// primitive datatypes 7 :types
+//String(call by value),Number,Boolean,null,undefined,Symbol(unique),BigInt
+
+//References(Non primitive) 3: types
+
+//Array,Objects,functions
+
+const score = 100
+let userEmail;
+const outsideTemp = null;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+//Array,Objects,Functions
+
+const heros = ["shaktiman","nagraj","dagro"];
+console.log(heros)
+//object
+
+let myobj = {
+    name: "Riya",
+    age: 21,
+}
+console.log(myobj)
+
+//function
+
+const myFunction = function()
+{
+    console.log("hello world");
+}
+myFunction()
+
+
+
+

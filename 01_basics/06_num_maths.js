cd64dd20ca5d1933474fab3bfb23fffa97bcd2b3
Riya sharma <121916673+riyasharma1709@users.noreply.github.com>
2025-05-02 12:39:14 +0000

    done

---
+++ 01_basics/06_num_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+console.log(score)
+
+const score_1 =new Number(400)
+console.log(score_1);
+
+console.log(score_1.toString());
+console.log(score_1.toString().length);
+console.log(score_1.toFixed(2));//point ni pachd ni value change karva mate
+
+console.log(score_1.toFixed(1));
+
+const other_num = 23.899
+console.log(other_num.toPrecision(2)); //round figure
+
+const num1 = 255.79
+console.log(num1.toPrecision(3));
+
+const num2 = 233.46
+console.log(num2.toPrecision(3));
+
+const num3 = 233.46
+console.log(num3.toPrecision(2));
+
+const num4 = 233.46
+console.log(num3.toPrecision(4));
+
+
+
+
+
+
+
+

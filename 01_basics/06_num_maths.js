e0eccfb19c1156ac9b75d8bfc5ef88ebb4211e1e
Riya sharma <121916673+riyasharma1709@users.noreply.github.com>
2025-05-02 14:16:53 +0000

    math

--- 01_basics/06_num_maths.js
+++ 01_basics/06_num_maths.js
@@ -25,10 +25,33 @@ console.log(num3.toPrecision(2));
 const num4 = 233.46
 console.log(num3.toPrecision(4));
 
+const hundreads = 10000000
+console.log(hundreads.toLocaleString('en-In'));
+
+//***************maths*****************
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(5.3));
+console.log(Math.floor(4.9));//give lower round value
+console.log(Math.max(4,3,28));
+console.log(Math.min(4,3,28));
+
+console.log(Math.random());//give value between 0 to 1
+console.log(Math.random()*10+1);//+1 value 1 thi upar leva mate 0 cas avoid karva mate
 
 
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1)) + min );// 10 thi upar value leva mate
 
+const change = 20
+console.log(change);
+let val1 = 10;
+console.log(val1)
 
+val1 = 50
+console.log(val1)
 
 
 

357c02fe623586029f1cb2dcb3d2670d0a0b1e36
Riya sharma <121916673+riyasharma1709@users.noreply.github.com>
2025-05-09 08:49:32 +0000

    done

---
+++ 01_basics/datein_js.js
@@ -0,0 +1,28 @@
+// let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+ let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());//month statrts with zero
+
+// let Datespecific = new Date("2023-01-14")
+// console.log(Datespecific.toLocaleString());
+// console.log(Datespecific.toString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let NewDate = new Date()
+console.log(NewDate);
+console.log(NewDate.getMonth()+1);
+`the date is ${NewDate.getDay()}`
+
+NewDate.toLocaleString('default',{
+    weekday:"long",
+})

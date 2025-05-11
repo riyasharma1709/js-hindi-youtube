98158fb8a5a9754403e87b116bbfa25189ebc24a
Riya sharma <121916673+riyasharma1709@users.noreply.github.com>
2025-05-11 09:31:29 +0000

    obj

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,43 @@
+//two type of declare object literal and constructor
+//singleton make with constructor
+//object.create (constructor method) 
+
+
+
+//object literals
+const my = Symbol("key2") //define symbol
+
+
+
+
+const jsUser = {
+    name :"Riya",
+    age : 21,
+    [my] : "mykey1",//for print symbol use squre braket
+    "full name" : "Riya Sharma",
+    email:"riya@gmail.com",
+    isLoggedIn:false,
+    lastLoggedIn:["Monday","friday"]
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"])
+//console.log(jsUser.full name) can't access like this
+console.log(jsUser["full name"])
+console.log(jsUser[my])
+
+jsUser.email = "hiesh@gmail.com" //to change value
+console.log(jsUser.email)
+//Object.freeze(jsUser)  //freeze used for lock the object value can't change
+jsUser.email = "sem@gmail.com"
+console.log(jsUser)
+
+jsUser.greeting = function(){
+    console.log("hello js user")
+}
+console.log(jsUser.greeting())
+
+jsUser.greetingTwo = function(){
+    console.log(`hello user,${this.name}`); //this keyword use to print property of object
+}
+console.log(jsUser.greetingTwo())

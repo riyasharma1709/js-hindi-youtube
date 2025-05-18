0114705e5381f082ccb841f06357e83232782006
Riya sharma <121916673+riyasharma1709@users.noreply.github.com>
2025-05-18 12:43:15 +0000

    d

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -5,7 +5,9 @@ marvel_heros.push(dc_heros)
 console.log(marvel_heros)
 console.log(marvel_heros[3][1])
 
+
 const allheros = marvel_heros.concat(dc_heros)
+console.log("after concate:")
 console.log(allheros)
 
 const all_new_heros = [...marvel_heros,...dc_heros] //spread operator

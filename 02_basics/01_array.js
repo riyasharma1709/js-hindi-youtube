22e0ec8d8c5a6c1e2d372122fb0224925c4753d6
Riya sharma <121916673+riyasharma1709@users.noreply.github.com>
2025-05-18 12:42:58 +0000

    do

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -11,16 +11,20 @@ console.log(myArray)
 myArray.pop()//last value remove karva mate
 
 console.log(myArray)
+console.log("shift:") //shift used for add value in first 
 myArray.unshift(9)
 console.log(myArray)
-myArray.shift()
+myArray.shift()//unshift used for remove value
 console.log(myArray)
 console.log(myArray.includes(9));
 console.log(myArray.indexOf(3));
+console.log("type of myarray:")
+console.log(typeof myArray)
 
 const newArr = myArray.join()//convert in string
 
 console.log(myArray)
+console.log("new array:")
 console.log(newArr)
 console.log(typeof newArr);
 
@@ -29,12 +33,13 @@ console.log(typeof newArr);
 
 
 console.log("A ",myArray);
-const myn1 = myArray.slice(1,3)//does not include last range
+const myn1 = myArray.slice(1,3)//print value does not include last range
 console.log(myn1)
 
 console.log("B ",myArray);
 
-const myn2  =myArray.splice(1,3)//it cut value include last range and change original array remove indicating portion
+const myn2  = myArray.splice(1,3)//it cut value include last range and change original array remove indicating portion
 console.log("c ",myArray);
+console.log("change array after splice:")
 console.log(myn2); 
 console.log(myArray)
